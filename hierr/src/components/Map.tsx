864bb6bfcfe615ba309405ffbb41f7ac023fce03
avenmia <mavend2@gmail.com>
2023-01-27 23:31:40 -1000

    migrating map

--- hierr/src/components/Map.tsx
+++ hierr/src/components/Map.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 export default function Map() {
   return (
-    <>
-      <h1>This is where the map will go!</h1>
-    </>
+    <div id="map">
+      <MapContainer center={[20.5, -157.510857]} zoom={7} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; "Map data © OpenStreetMap contributors, Esri Community Maps contributors, Map layer by Esri'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+        />
+        <Marker position={[20.5, -157.510857]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   )
 }
