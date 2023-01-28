5f9d9a675f7b8a99673740775bafbdcbdc08ce84
avenmia <mavend2@gmail.com>
2023-01-28 12:09:34 -1000

    Adding commented out map code to migrate

--- hierr/src/components/Map.tsx
+++ hierr/src/components/Map.tsx
@@ -6,6 +6,94 @@ import 'leaflet/dist/leaflet.css';
 import { GeoJSON } from 'react-leaflet';
 import type {GeoJsonObject, Feature, Geometry} from "geojson"
 
+// TODO: Migrate the following:
+//! select the basemap layer using the dropdown
+// function updateBaseMap(baseMapSelection) {
+// 	if (baseMap) {
+// 		map.removeLayer(baseMap);
+// 	}
+// 	switch (baseMapSelection) { 
+// 		case "osm": 
+// 			baseMap = L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+// 			break;
+// 		case "esri_light":
+// 			baseMap = L.tileLayer(`https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}`, {attribution: "Map data © OpenStreetMap contributors, Esri Community Maps contributors, Map layer by Esri"}).addTo(map);
+// 			break;
+// 		case "esri_dark":
+// 			baseMap = L.tileLayer(`https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}`, {attribution: "Map data © OpenStreetMap contributors, Esri Community Maps contributors, Map layer by Esri"}).addTo(map);
+// 			break;
+// 		case "carto_dark":
+// 			baseMap = L.tileLayer(`https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'`, {attribution:
+// 			'&copy; <a href="https://www.openstreetmap.org/copyright">CartoDB Dark</a> contributors'}).addTo(map);
+// 			break;
+// 		default:
+// 			baseMap = L.tileLayer(`https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}`, {attribution: "Map data © OpenStreetMap contributors, Esri Community Maps contributors, Map layer by Esri"}).addTo(map);
+// 			break;	
+// 	}
+// }
+
+// async function addCensusTracts() {
+// 	const response = await fetch('/census-tracts_min.geojson')
+// 	const data = await response.json()
+
+// 	//! create a layer from the response 
+// 	var geojson = L.geoJson(data, {
+// 		style: function (feature) {
+// 			//! conditionally color each tract based on the ['pop20'] t (population 2020)
+// 			//! more info Census Blocks ['pop20'] here: http://proximityone.com/geo_blocks.html
+
+// 			// TODO: add more fine grained conditions for color palette | Maybe-TODO: possibly change the the if/else to a switch statement
+// 			var color
+// 			var pop20 = feature.properties['pop20']
+// 			if (pop20 <= 2500) {
+// 				color = '#bd93f9'
+// 			} else if (pop20 < 5000 && pop20 > 2500) {
+// 				color = '#ffb86c'
+// 			} else if (feature.properties['pop20'] < 10000 && feature.properties['pop20'] > 5000) {
+// 				color = '#8be9fd'
+// 			}
+// 			//
+// 			return {
+// 				fillColor: color,
+// 				color: '#44475a',
+// 				weight: 0.5,
+// 				opacity: 1,
+// 				fillOpacity: 0.4,
+// 			}
+// 		},
+
+// 		//! change fill opacity on mouse in and out
+// 		onEachFeature: function (feature, layer) {
+// 			layer.on({
+// 					mouseover: function (e) {
+// 							var layer = e.target;
+// 							layer.setStyle({
+// 									fillOpacity: 0.8,
+// 							});
+// 							let name = e.target.feature.properties['tractname']
+// 							layer.bindPopup(name).openPopup();
+// 					},
+// 					mouseout: function (e) {
+// 							geojson.resetStyle(layer);
+// 							layer.closePopup();
+// 					},
+// 			});
+// 		}
+// 	}).addTo(map)
+// }
+// addCensusTracts();
+
+// var popup = L.popup();
+// // popup on click that shows lat/long
+// // TODO: add users census tract info to polis embed
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent(e.latlng.toString())
+//         .openOn(map);
+// }
+
+// map.on('click', onMapClick);
 
 export default function Map() {
   const getCensusTractFillColor = (population2020: number) => {
