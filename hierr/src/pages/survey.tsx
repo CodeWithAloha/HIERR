5df298de7e1ceb80fd464b6c44173cfa2fba2049
avenmia <mavend2@gmail.com>
2023-01-30 21:16:30 -1000

    Adding survey components

---
+++ hierr/src/pages/survey.tsx
@@ -0,0 +1,9 @@
+import dynamic from "next/dynamic";
+
+const Survey = dynamic(() => import('../components/survey/demographicssurvey'), {
+  ssr: false
+})
+
+export default function SurveyPage() {
+  return (<Survey/>)
+}
