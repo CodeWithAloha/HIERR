d90e184562424ce46f15590afa3bbf4de8ba1e8b
avenmia <mavend2@gmail.com>
2023-02-02 07:41:53 -1000

    Add magic link email auth (#15)
    * Adding nodemailer
    
    * Add email provider code
    
    * removing discord provider
    
    * Adding README to set up smtp server
    
    * Removing discord variables from schema
    
    * Update src/env/schema.mjs
    
    Co-authored-by: Tyler Chong <tyliec@github.com>
    
    ---------
    
    Co-authored-by: Tyler Chong <tyliec@github.com>

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -19,8 +19,7 @@ export const serverSchema = z.object({
     // VERCEL_URL doesn't include `https` so it cant be validated as a URL
     process.env.VERCEL ? z.string() : z.string().url(),
   ),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
+  CLIENT_ID: z.string(),
 });
 
 /**
@@ -33,8 +32,7 @@ export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  CLIENT_ID: process.env.CLIENT_ID || "TestClientID",
 };
 
 /**
