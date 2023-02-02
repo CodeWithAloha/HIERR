29049717a71a901af41677388fcb6387069873a4
avenmia <mavend2@gmail.com>
2023-02-01 14:44:30 -1000

    removing discord provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,10 +4,8 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "../env/server.mjs";
 import { prisma } from "./db";
 
 /**
@@ -51,10 +49,6 @@ export const authOptions: NextAuthOptions = {
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM
-    }),
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here
