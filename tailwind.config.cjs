a8489784467bc74e4ceeb9286b035c296864c0eb
avenmia <mavend2@gmail.com>
2023-02-03 15:21:20 -1000

    Adding tailwind color scheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      Green: "#1A9432",
+      Blue: "#3276AE",
+      White: "#FFFFFF",
+      Gray: "#4C4C4C"
+    }
   },
   plugins: [],
 };
