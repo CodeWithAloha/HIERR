6e7a4bf384cf3b8f347e7adf6dab991ee7e0edcf
avenmia <mavend2@gmail.com>
2023-02-03 15:22:20 -1000

    Removing background gradient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Login: NextPage = () => {
      integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
      async />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center ">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">Welcome to HIERR</h1>
           <div className="flex flex-col items-center gap-2">
