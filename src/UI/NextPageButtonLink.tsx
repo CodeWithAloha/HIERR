707b2d0ffec43b6de3ec4a7c7fb9c6c77994a233
avenmia <mavend2@gmail.com>
2023-02-03 16:18:18 -1000

    Adding disabled option to NextPageButtonLink

--- src/UI/NextPageButtonLink.tsx
+++ src/UI/NextPageButtonLink.tsx
@@ -3,13 +3,14 @@ import Link from "next/link"
 interface NextPageButtonLinkProps{
   pageName: string;
   msg: string;
+  disabled?: boolean;
 }
 
-export const NextPageButtonLink = ({pageName, msg}: NextPageButtonLinkProps) => {
+export const NextPageButtonLink = ({pageName, msg, disabled}: NextPageButtonLinkProps) => {
   const href = `./${pageName}`;
   return (
   <div>
-    <Link href={href}><button className="rounded-full bg-white/10 px-10 py-3 hover:bg-white/20 my-10 text-white">
+    <Link href={href}><button disabled={disabled} className="rounded-full bg-white/10 px-10 py-3 hover:bg-white/20 my-10 text-white">
             {msg}
     </button></Link>
   </div>
