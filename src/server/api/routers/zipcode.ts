48a67f255ecdfa46354fcb54a7bd2cf0a7b65bd9
avenmia <mavend2@gmail.com>
2023-02-05 15:35:22 -1000

    Fixing zipcode query code

--- src/server/api/routers/zipcode.ts
+++ src/server/api/routers/zipcode.ts
@@ -2,31 +2,22 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
-interface ZipCode {
-  id: string;
-  userId: string;
-  zipcode: string;
-}
-
 export const zipcodeRouter = createTRPCRouter({
   postZipCode: publicProcedure
     .input(z.object({ userId: z.string(), zipcode: z.string() }))
-    .mutation(async ({ input, ctx }) => {
+    .mutation(({ input, ctx }) => {
       if(!ctx.session){
         console.log("Not authenticated")
         return null;
       }
       const { id: userId } = ctx.session.user
       const zipcode = input.zipcode;
-      const post: ZipCode = await ctx.prisma.post.create({
+      return ctx.prisma.zipCode.create({
         data: {
           userId,
           zipcode
         }
-      }) as ZipCode;
-      return {
-        zipcode: `User: ${input.userId} zipcode: ${input.zipcode}`,
-      };
+      });
     }),
 
   // getAllZipCodes: publicProcedure.query(({ ctx }) => {
