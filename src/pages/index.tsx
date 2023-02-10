45db23e12de0135f7a61ebddd036e50f9e2334be
Lydia Sollis <lydia.j.sollis@gmail.com>
2023-02-09 20:45:10 -1000

    Update index.tsx
    delete reference to example router in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,7 +51,6 @@ const AuthShowcase: React.FC = () => {
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
         {
           sessionData ?
           <NextPageButtonLink pageName="censusmap" msg="Ready to start the survey? Click here!" />
