de974065f18a60b1d3e0c11367d5581957f0421f
avenmia <mavend2@gmail.com>
2023-02-11 15:33:42 -1000

    avenmia/AddCensusTractFunctionality (#33)
    * Adding census tract selection to map
    
    * Adding census tract model and user field
    
    * Adding user router
    
    * Adding user router functionality
    
    * Users can click on the census tract map to post their census tract

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "./trpc";
 import { zipcodeRouter } from "./routers/zipcode";
+import { userRouter } from "./routers/user";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +8,8 @@ import { zipcodeRouter } from "./routers/zipcode";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  zipcode: zipcodeRouter
+  zipcode: zipcodeRouter,
+  user: userRouter
 });
 
 // export type definition of API
