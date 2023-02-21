17ffd888732660b05060dc7ee162faa9da686c1e
Kurt Malley <kjmalley7@gmail.com>
2023-02-21 10:06:17 -1000

    refactor: changing tilelayer to use OpenStreetMap tiles

--- src/components/censusmap.tsx
+++ src/components/censusmap.tsx
@@ -90,49 +90,47 @@ const CensusTractMap: NextPage = () => {
   };
 
   return (
-    <div className="flex h-screen flex-col items-center bg-[#3276AE]">
-      {existingCensusTractId ? (
-        <ExistingCensusMap existingCensusTract={existingCensusTractId} />
-      ) : userCensusTract ? (
-        <CompletedCensusMap userSelectedCensusTract={userCensusTract} />
-      ) : (
-        <>
-          <h1 className="mt-4 w-3/4 items-center pb-5 pt-5 text-3xl text-white">
-            Please select the census tract area where you reside
-            <p className="text-xl text-white">
-              Use the ➕ and ➖ on the map to find your tract
-            </p>
-          </h1>
-          <div
-            id="map"
-            className="w-3/4 rounded-sm border-8 border-solid border-gray"
-          >
-            <MapContainer
-              center={[21.43805, -157.985262]}
-              zoom={11}
-              scrollWheelZoom={true}
-              style={{ height: "800px" }}
-            >
-              <TileLayer
-                attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-                url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png"
-              />
-              <GeoJSONComponent
-                data={CensusTractData as GeoJsonObject}
-                style={(
-                  val: Feature<Geometry, { pop20: number }> | undefined
-                ) => censusTractStyle(val)}
-                onEachFeature={(feature, layer) =>
-                  handleFeature(feature, layer)
-                }
-                ref={geoJsonRef as Ref<any>}
-              />
-            </MapContainer>
-          </div>
-        </>
-      )}
-    </div>
-  );
+		<div className='flex h-screen flex-col items-center bg-[#3276AE]'>
+			{existingCensusTractId ? (
+				<ExistingCensusMap existingCensusTract={existingCensusTractId} />
+			) : userCensusTract ? (
+				<CompletedCensusMap userSelectedCensusTract={userCensusTract} />
+			) : (
+				<>
+					<h1 className='mt-4 w-3/4 items-center pb-5 pt-5 text-3xl text-white'>
+						Please select the census tract area where you reside
+						<p className='text-xl text-white'>
+							Use the ➕ and ➖ on the map to find your tract
+						</p>
+					</h1>
+					<div
+						id='map'
+						className='w-3/4 rounded-sm border-8 border-solid border-gray'>
+						<MapContainer
+							center={[21.43805, -157.985262]}
+							zoom={11}
+							scrollWheelZoom={true}
+							style={{ height: '800px' }}>
+							<TileLayer
+								attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+								url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+							/>
+							<GeoJSONComponent
+								data={CensusTractData as GeoJsonObject}
+								style={(
+									val: Feature<Geometry, { pop20: number }> | undefined
+								) => censusTractStyle(val)}
+								onEachFeature={(feature, layer) =>
+									handleFeature(feature, layer)
+								}
+								ref={geoJsonRef as Ref<any>}
+							/>
+						</MapContainer>
+					</div>
+				</>
+			)}
+		</div>
+	)
 };
 
 export default CensusTractMap;
