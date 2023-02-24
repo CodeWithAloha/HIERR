ab3b7887691982ffbe3a4c16b24030807f34967f
avenmia <mavend2@gmail.com>
2023-02-24 13:55:27 -1000

    Avenmia/connect survey to db (#54)
    * Pulling questions and answers for the survey from the database
    
    * Fixing answer sorting
    
    * Adding add user answer logic
    
    * Adding question position to set survey order
    
    * Fixing compile error
    
    * Update src/pages/polis.tsx
    
    Co-authored-by: Tyler Chong <tyliec@github.com>
    
    * Code cleanup
    
    ---------
    
    Co-authored-by: Tyler Chong <tyliec@github.com>

--- src/components/survey/demographicssurvey.tsx
+++ src/components/survey/demographicssurvey.tsx
@@ -2,25 +2,29 @@ import React, { useCallback, useMemo, useState } from "react";
 import SurveyQuestion from "./surveyquestion";
 import Link from "next/link";
 import { NextPageButtonLink } from "../../UI/NextPageButtonLink";
+import { api } from "../../utils/api";
 
 interface SurveyData {
+  questionId: string;
   question: string;
-  answers: string[]
+  answers: SurveyAnswer[]
+}
+
+interface SurveyAnswer {
+  answer: string;
+  answerId: string;
 }
 
 export type QuestionDirection = "Prev" | "Next"
 
 export default function DemographicsSurvey() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [surveyCompleted, setSurveyCompleted] = useState(false)
+  const [surveyCompleted, setSurveyCompleted] = useState(false);
+  const surveyDataDB = (api.survey.getSurveyData.useQuery()?.data  ?? []).sort((d1, d2) => d1.position - d2.position );
+  const surveyData: SurveyData[] = surveyDataDB.map(sd => {return {questionId: sd.id, question: sd.question, answers: sd.answers.sort((a1, a2) => a1.position - a2.position).map(a => {return {answer: a.answer, answerId: a.id}})}})
 
-  const surveyData: SurveyData[] = [
-    {question: "How are you today?", answers:["very bad", "bad", "neutral", "good", "very good"]},
-    {
-      question: "Does pineapple belong on pizza?", answers:["yes", "no"]
-    },
-    {question: "On a scale of 1 - 5, how well can you draw?", answers: ["1", "2", "3", "4", "5"]}
-  ]
+  const postUserAnswer = api.survey.addUserAnswer.useMutation();
+
   const userAnswers: string[] = useMemo(() => [], []);
   // TODO: make sure questions do not go out of bounds
   const updateCurrentQuestion = useCallback((change: QuestionDirection, answer?: string) => {
@@ -28,14 +32,21 @@ export default function DemographicsSurvey() {
       setCurrentQuestion(currentQuestion - 1);
       return;
     }
+    const questionId = surveyData[currentQuestion]?.questionId;
+    const answerId = surveyData[currentQuestion]?.answers.find(a => a.answer === answer)?.answerId;
     if(currentQuestion === surveyData.length - 1) {
       userAnswers.push(answer ?? "")
       console.log("User answers are:", userAnswers)
+      // TODO: Fix these conditionals
+      postUserAnswer.mutate({answerId: answerId ?? "", questionId: questionId ?? ""})
       setSurveyCompleted(true)
       // TODO: Send user answers to database
       return;
     }
     userAnswers.push(answer ?? "")
+
+    // TODO: Fix these conditionals
+    postUserAnswer.mutate({answerId: answerId ?? "", questionId: questionId ?? ""})
     setCurrentQuestion(currentQuestion + 1)
   },[currentQuestion, userAnswers, surveyData.length]);
 
@@ -56,7 +67,7 @@ export default function DemographicsSurvey() {
         {
           surveyData[currentQuestion]!== undefined ? 
           // TODO: Fix these conditionals
-          <SurveyQuestion question={surveyData[currentQuestion]?.question ?? ""} answers={surveyData[currentQuestion]?.answers ?? []} updateQuestion={updateCurrentQuestion} /> : null
+          <SurveyQuestion question={surveyData[currentQuestion]?.question ?? ""} answers={surveyData[currentQuestion]?.answers.map(a => a.answer) ?? []} updateQuestion={updateCurrentQuestion} /> : null
         }
       </>
     }
