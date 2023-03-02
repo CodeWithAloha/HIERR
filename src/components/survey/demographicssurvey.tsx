6d13bf5efcfe95475b055ad48d0f16726cefd9f3
avenmia <mavend2@gmail.com>
2023-03-01 16:37:02 -1000

    Removing posting user answers and fixing survey out of bounds bug

--- src/components/survey/demographicssurvey.tsx
+++ src/components/survey/demographicssurvey.tsx
@@ -18,11 +18,12 @@ export interface SurveyAnswer {
 }
 
 export type QuestionDirection = "Prev" | "Next";
-export type QuestionType = "option" | "multiSelect" | "text";
+export type QuestionType = "option" | "multiSelect" | "text" | "number";
 export type AnswerType = "option" | "text" | "number" | "optionText";
 
 export default function DemographicsSurvey() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswer, setUserAnswer] = useState("");
   const [surveyCompleted, setSurveyCompleted] = useState(false);
   const surveyDataDB = (api.survey.getSurveyData.useQuery()?.data  ?? []).sort((d1, d2) => d1.position - d2.position );
   const surveyData: SurveyData[] = surveyDataDB.map(sd => {return {questionId: sd.id, question: sd.question, questionType: (sd.questionType as QuestionType), answers: sd.answers.sort((a1, a2) => a1.position - a2.position).map(a => {return {answer: a.answer, answerId: a.id, answerType: a.answerType}})}})
@@ -31,25 +32,31 @@ export default function DemographicsSurvey() {
 
   const userAnswers: string[] = useMemo(() => [], []);
   // TODO: make sure questions do not go out of bounds
-  const updateCurrentQuestion = useCallback((change: QuestionDirection, answer?: string) => {
-    if(change === "Prev") {
+  const updateCurrentQuestion = useCallback((change: QuestionDirection, answer?: string, answerType?: string) => {
+    if(change === "Prev" && currentQuestion !== 0) {
       setCurrentQuestion(currentQuestion - 1);
       return;
     }
-    const questionId = surveyData[currentQuestion]?.questionId;
-    const answerId = surveyData[currentQuestion]?.answers.find(a => a.answer === answer)?.answerId;
-    if(currentQuestion === surveyData.length - 1) {
-      userAnswers.push(answer ?? "")
-      // TODO: Fix these conditionals
-      postUserAnswer.mutate({answerId: answerId ?? "", questionId: questionId ?? ""})
-      setSurveyCompleted(true)
-      // TODO: Send user answers to database
+
+    if(change === "Prev" && currentQuestion === 0) {
       return;
     }
-    userAnswers.push(answer ?? "")
+    // const questionId = surveyData[currentQuestion]?.questionId;
+    // const foundAnswer = surveyData[currentQuestion]?.answers.find(a => a.answer === answer);
+    
+    // TODO: Remove guaranteed not null exclamation point
+    // const {answerId, answerValue } = foundAnswer!;
+    if(currentQuestion === surveyData.length - 1) {
+      // userAnswers.push(answer ?? "")
+      // TODO: Fix these conditionals
+      // postUserAnswer.mutate({answerId: answerId ?? "", questionId: questionId ?? ""})
+      // setSurveyCompleted(true)
+      return;
+    }
+    // userAnswers.push(answer ?? "")
 
     // TODO: Fix these conditionals
-    postUserAnswer.mutate({answerId: answerId ?? "", questionId: questionId ?? ""})
+    // postUserAnswer.mutate({answerId: answerId ?? "", questionId: questionId ?? ""})
     setCurrentQuestion(currentQuestion + 1)
   },[currentQuestion, userAnswers, surveyData.length]);
 
