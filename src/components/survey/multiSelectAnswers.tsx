3e8ed391d30907ea024b0a4304b6dca90d7b1f1e
avenmia <mavend2@gmail.com>
2023-03-02 17:46:11 -1000

    Adding free text to multiselect answers

--- src/components/survey/multiSelectAnswers.tsx
+++ src/components/survey/multiSelectAnswers.tsx
@@ -1,17 +1,42 @@
 import { MouseEvent, useRef, useState } from "react";
-import { AnswerType } from "./demographicssurvey";
+import { AnswerType, SurveyAnswer } from "./demographicssurvey";
 
 interface MultiSelectAnswersProps {
-  answers: {answer: string, answerType: AnswerType}[];
+  answers: SurveyAnswer[];
   updateCurrentAnswer: (val: string) => void;
 }
 export default function MultiSelectAnswers({answers, updateCurrentAnswer}: MultiSelectAnswersProps) {
   const ref = useRef<HTMLDivElement>(null);
   const handleClick = () => {
-  const checkboxes = (ref.current as HTMLDivElement).querySelectorAll('[type=checkbox]');
-  const checkedAnswers = Array.from(checkboxes).filter(cb => (cb as HTMLInputElement).checked);
+  const checkboxesNoText = (ref.current as HTMLDivElement).querySelectorAll('[type=checkbox]:not([id*=optionText])');
+  const checkboxesText = (ref.current as HTMLDivElement).querySelectorAll('[type=checkbox][id*=optionText]');
+  const checkedAnswers = Array.from(checkboxesNoText).filter(cb => (cb as HTMLInputElement).checked);
+  const checkedTextAnswers = Array.from(checkboxesText).filter(cb => (cb as HTMLInputElement).checked);
   const values = checkedAnswers.map(ca => (ca as HTMLInputElement).value).join(";")
+  const textValues = checkedTextAnswers.map(ca => (ca as HTMLInputElement).value).join(";")
+  console.log("TEXT VALUES:", checkedTextAnswers)
+  console.log("TEXT VALUES2:", textValues)
+  
   updateCurrentAnswer(values)
+  }
+
+  const checkBox = (a: SurveyAnswer, index: number) => {
+    return (
+      <>
+       <input type="checkbox" id={`a-${index}-option`} name={`a-${index}`} value={a.answer} onClick={() => handleClick()} />
+          <label htmlFor={`a-${index}`}>{a.answer}</label><br/>
+      </>
+    )
+  }
+
+  // TODO: Fix this
+  const checkBoxText = (a: SurveyAnswer, index: number) => {
+    return (
+      <>
+       <input type="checkbox" id={`a-${index}-optionText`} name={`a-${index}-optionText`} value={a.answer} onClick={() => handleClick()} />
+          <label htmlFor={`a-${index}-optionText`}>{a.answer} <input className="border-2 border-rose-500" id={`${index}-userText`} type={"text"}></input></label><br/>
+      </>
+    )
   }
 
   return (
@@ -21,8 +46,7 @@ export default function MultiSelectAnswers({answers, updateCurrentAnswer}: Multi
           (a, index) => {
             return (
               <div key={index}>
-              <input type="checkbox" id={`a-${index}`} name={`a-${index}`} value={a.answer} onClick={() => handleClick()} />
-              <label htmlFor={`a-${index}`}>{a.answer}</label><br/>
+              {a.answerType === "option" ? checkBox(a, index) : a.answerType === "optionText" ? checkBoxText(a, index) : null}
               </div>
             )
           }
