92d8c73c2a6bd7ea613039555c94b4e214614fc4
l7ana <liana@saedesigngroup.com>
2023-03-04 13:30:09 -1000

    adds conditional pattern

--- src/components/survey/textAnswers.tsx
+++ src/components/survey/textAnswers.tsx
@@ -5,16 +5,9 @@ interface TextAnswerProps {
 
 export default function TextAnswer({ updateCurrentAnswer, number}: TextAnswerProps) {
 
-  if (number) {
-    return (
-      <>
-          <input className="border-2 border-rose-500" type="text" id="textQuestion" name="textQuestion" 
-                pattern="^1?[0-9]{1,2}$1[0-9][0-9]" title="Please enter a number." onChange={(e) => updateCurrentAnswer(e.target.value)} />
-      </>
-    )
-  } return (
+  return (
     <>
-        <input className="border-2 border-rose-500" type="text" id="textQuestion" name="textQuestion" onChange={(e) => updateCurrentAnswer(e.target.value)} />
+        <input className="border-2 border-rose-500" type="text" id="textQuestion" name="textQuestion" pattern={number ? "^1?[0-9]{1,2}$1[0-9][0-9]" : ""} title={number ? "Please enter a number." : ""} onChange={(e) => updateCurrentAnswer(e.target.value)} />
     </>
   )
 }
