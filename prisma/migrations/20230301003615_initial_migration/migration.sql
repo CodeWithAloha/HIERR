1313fe58fec249e6f876f1b85f33819f8f388c84
avenmia <mavend2@gmail.com>
2023-03-04 15:10:14 -1000

    Updating migration

--- prisma/migrations/20230301003615_initial_migration/migration.sql
+++ prisma/migrations/20230301003615_initial_migration/migration.sql
@@ -143,6 +143,8 @@ DECLARE @questionId10 uniqueidentifier = NEWID();
 DECLARE @questionId11 uniqueidentifier = NEWID();
 DECLARE @questionId12 uniqueidentifier = NEWID();
 DECLARE @questionId13 uniqueidentifier = NEWID();
+DECLARE @questionId14 uniqueidentifier = NEWID();
+DECLARE @questionId15 uniqueidentifier = NEWID();
 
 DECLARE @answerId1 uniqueidentifier = NEWID();
 
@@ -234,6 +236,11 @@ DECLARE @answerId64 uniqueidentifier = NEWID();
 DECLARE @answerId65 uniqueidentifier = NEWID();
 DECLARE @answerId66 uniqueidentifier = NEWID();
 DECLARE @answerId67 uniqueidentifier = NEWID();
+DECLARE @answerId68 uniqueidentifier = NEWID();
+DECLARE @answerId69 uniqueidentifier = NEWID();
+DECLARE @answerId70 uniqueidentifier = NEWID();
+DECLARE @answerId71 uniqueidentifier = NEWID();
+DECLARE @answerId72 uniqueidentifier = NEWID();
 
 
 INSERT INTO
@@ -262,50 +269,61 @@ VALUES
     ),
     (
         @questionId6,
-        'Do you currently have, or did you have a non-dial-up internet subscription (i.e., cellular data, cable, fiber optic, DSL, satellite) during the pandemic?',
+        'Do you currently have a non-dial-up internet subscription (i.e., cellular data, cable, fiber optic, DSL, satellite)?',
         6, 'option'
     ),
     (
         @questionId7,
-        'Do you currently have, or did you have health insurance during the pandemic? 
-',
+        'Did you have a non-dial-up internet subscription (i.e., cellular data, cable, fiber optic, DSL, satellite) during the pandemic?',
         7, 'option'
     ),
     (
         @questionId8,
-        'Are you currently living in a… (Select one answer)
+        'Do you currently have health insurance? 
 ',
         8, 'option'
     ),
     (
         @questionId9,
-        'Is your housing structure above… (Select one answer)
+        'Did you have health insurance during the pandemic? 
 ',
         9, 'option'
-    )
-,
+    ),
     (
         @questionId10,
-        'What is your ethnic identification? If more than one ethnicity, which do you identify most. 
-If you cannot choose, please select mixed. If you have any Hawaiian in your ethnic mix, please select Hawaiian/Part Hawaiian
+        'Are you currently living in a… (Select one answer)
 ',
         10, 'option'
     ),
     (
         @questionId11,
-        'What is the highest level of education you have completed? (Select one answer)
+        'Is your housing structure below… (Select one answer)
 ',
         11, 'option'
-    ),
+    )
+,
     (
         @questionId12,
-        'Do you identify with having a disability?',
+        'What is your ethnic identification? If more than one ethnicity, which do you identify most. 
+If you cannot choose, please select mixed. If you have any Hawaiian in your ethnic mix, please select Hawaiian/Part Hawaiian
+',
         12, 'option'
     ),
-(
+    (
         @questionId13,
-        'What was your household income in 2022, before taxes? Please consider and include in your thinking your best estimate of the income of all persons living in your household. (Select one answer)',
+        'What is the highest level of education you have completed? (Select one answer)
+',
         13, 'option'
+    ),
+    (
+        @questionId14,
+        'Do you identify with having a disability?',
+        14, 'option'
+    ),
+(
+        @questionId15,
+        'What was your household income in 2022, before taxes? Please consider and include in your thinking your best estimate of the income of all persons living in your household. (Select one answer)',
+        15, 'option'
     )
 INSERT INTO
     [dbo].[SurveyAnswer] (id, questionId, answer, position, answerType)
@@ -317,7 +335,7 @@ VALUES
     (@answerId5, @questionId3, 'Male', 2, 'option'),
     (@answerId6, @questionId3, 'Other,', 3, 'optionText'),
     (@answerId7, @questionId4, '', 1, 'number'),
-    (@answerId8, @questionId5, 'Employed fulltime / part time', 1, 'option'),
+    (@answerId8, @questionId5, 'Employed full time / part time', 1, 'option'),
     (@answerId9, @questionId5, 'Unemployed, not looking for work', 2, 'option'),
     (@answerId10, @questionId5, 'Unemployed, looking for work', 3, 'option'),
     (@answerId11, @questionId5, 'Retired', 4, 'option'),
@@ -329,54 +347,58 @@ VALUES
     (@answerId17, @questionId6, 'no', 2, 'option'),
     (@answerId18, @questionId7, 'yes', 1, 'option'),
     (@answerId19, @questionId7, 'no', 2, 'option'),
-    (@answerId20, @questionId8, 'Permanent stable housing structure (e.g., house, apartment, condominium, accessory dwelling unit / ADU, ohana unit)', 1, 'option'),
-    (@answerId21, @questionId8, 'Mobile home (e.g., RV, vehicle, boat)', 2, 'option'),
-    (@answerId22, @questionId8, 'Group quarters (e.g., institutional facility, college dorms, military housing, correctional facility, transitional housing)', 3, 'option'),
-    (@answerId23, @questionId8, 'Other permanent housing (e.g., community village, tiny home)', 4, 'option'),
-    (@answerId24, @questionId8, 'I am houseless', 5, 'option'),
-    (@answerId25, @questionId8, 'Other,', 6, 'optionText'),
-    (@answerId26, @questionId9, 'Owned by you or someone in the household, with mortgage / loan', 1, 'option'),
-    (@answerId27, @questionId9, 'Owned by you or someone in the household, without mortgage / loan', 2, 'option'),
-    (@answerId28, @questionId9, 'Rented by you or someone in the household', 3, 'option'),
-    (@answerId29, @questionId9, 'Rent free to you and your household', 4, 'option'),
-    (@answerId30, @questionId9, 'Not applicable', 5, 'option'),
-    (@answerId31, @questionId10, 'Caucasian / White', 1, 'option'),
-    (@answerId32, @questionId10, 'Black or African American', 2, 'option'),
-    (@answerId33, @questionId10, 'American Indian or Alaska Native,', 3, 'optiontText'),
-    (@answerId34, @questionId10, 'Asian Indian', 4, 'option'),
-    (@answerId35, @questionId10, 'Japanese', 5, 'option'),
-    (@answerId36, @questionId10, 'Native Hawaiian', 6, 'option'),
-    (@answerId37, @questionId10, 'Chinese', 7, 'option'),
-    (@answerId38, @questionId10, 'Korean', 8, 'option'),
-    (@answerId39, @questionId10, 'Guamanian or Chamorro', 9, 'option'),
-    (@answerId40, @questionId10, 'Filipino', 10, 'option'),
-    (@answerId41, @questionId10, 'Vietnamese', 11, 'option'),
-    (@answerId42, @questionId10, 'Samoan', 12, 'option'),
-    (@answerId43, @questionId10, 'Mixed (not Hawaiian)', 13, 'option'),
-    (@answerId44, @questionId10, 'Other Asian,', 14, 'optionText'),
-    (@answerId45, @questionId10, 'Other Pacific Islander,', 15, 'optionText'),
-    (@answerId46, @questionId10, 'Some other race,', 16, 'optionText'),
-    (@answerId47, @questionId10, 'Prefer not to say', 17, 'option'),
-    (@answerId48, @questionId11, 'Less than high school', 1, 'option'),
-    (@answerId49, @questionId11, 'High school/GED', 2, 'option'),
-    (@answerId50, @questionId11, 'Some college, no degree', 3, 'option'),
-    (@answerId51, @questionId11, 'Trade school', 4, 'option'),
-    (@answerId52, @questionId11, 'Associate degree', 5, 'option'),
-    (@answerId53, @questionId11, 'Bachelor’s degree', 6, 'option'),
-    (@answerId54, @questionId11, 'Graduate or professional degree', 7, 'option'),
-    (@answerId55, @questionId12, 'yes', 1, 'option'),
-    (@answerId56, @questionId12, 'no', 2, 'option'),
-    (@answerId57, @questionId13, 'Less than $10,000', 1, 'option'),
-    (@answerId58, @questionId13, '$10,000 but less than $15,000', 2, 'option'),
-    (@answerId59, @questionId13, '$15,000 but less than $25,000', 3, 'option'),
-    (@answerId60, @questionId13, '$25,000 but less than $35,000', 4, 'option'),
-    (@answerId61, @questionId13, '$35,000 but less than $50,000', 5, 'option'),
-    (@answerId62, @questionId13, '$50,000 but less than $75,000', 6, 'option'),
-    (@answerId63, @questionId13, '$75,000 but less than $100,000', 7, 'option'),
-    (@answerId64, @questionId13, '$100,000 but less than 150,000', 8, 'option'),
-    (@answerId65, @questionId13, '$150,000 but less than $200,000', 9, 'option'),
-    (@answerId66, @questionId13, '$200,000 or more', 10, 'option'),
-    (@answerId67, @questionId13, 'Don’t know/Prefer not to answer', 11, 'option')
+    (@answerId20, @questionId8, 'yes', 1, 'option'),
+    (@answerId21, @questionId8, 'no', 2, 'option'),
+    (@answerId22, @questionId9, 'yes', 1, 'option'),
+    (@answerId23, @questionId9, 'no', 2, 'option'),
+    (@answerId24, @questionId10, 'Permanent stable housing structure (e.g., house, apartment, condominium, accessory dwelling unit / ADU, ohana unit)', 1, 'option'),
+    (@answerId25, @questionId10, 'Mobile home (e.g., RV, vehicle, boat)', 2, 'option'),
+    (@answerId26, @questionId10, 'Group quarters (e.g., institutional facility, college dorms, military housing, correctional facility, transitional housing)', 3, 'option'),
+    (@answerId27, @questionId10, 'Other permanent housing (e.g., community village, tiny home)', 4, 'option'),
+    (@answerId28, @questionId10, 'I am houseless', 5, 'option'),
+    (@answerId29, @questionId10, 'Other,', 6, 'optionText'),
+    (@answerId30, @questionId11, 'Owned by you or someone in the household, with mortgage / loan', 1, 'option'),
+    (@answerId31, @questionId11, 'Owned by you or someone in the household, without mortgage / loan', 2, 'option'),
+    (@answerId32, @questionId11, 'Rented by you or someone in the household', 3, 'option'),
+    (@answerId33, @questionId11, 'Rent free to you and your household', 4, 'option'),
+    (@answerId34, @questionId11, 'Not applicable', 5, 'option'),
+    (@answerId35, @questionId12, 'Caucasian / White', 1, 'option'),
+    (@answerId36, @questionId12, 'Black or African American', 2, 'option'),
+    (@answerId37, @questionId12, 'American Indian or Alaska Native,', 3, 'optiontText'),
+    (@answerId38, @questionId12, 'Asian Indian', 4, 'option'),
+    (@answerId39, @questionId12, 'Japanese', 5, 'option'),
+    (@answerId40, @questionId12, 'Native Hawaiian', 6, 'option'),
+    (@answerId41, @questionId12, 'Chinese', 7, 'option'),
+    (@answerId42, @questionId12, 'Korean', 8, 'option'),
+    (@answerId43, @questionId12, 'Guamanian or Chamorro', 9, 'option'),
+    (@answerId44, @questionId12, 'Filipino', 10, 'option'),
+    (@answerId45, @questionId12, 'Vietnamese', 11, 'option'),
+    (@answerId46, @questionId12, 'Samoan', 12, 'option'),
+    (@answerId47, @questionId12, 'Mixed (not Hawaiian)', 13, 'option'),
+    (@answerId48, @questionId12, 'Other Asian,', 14, 'optionText'),
+    (@answerId49, @questionId12, 'Other Pacific Islander,', 15, 'optionText'),
+    (@answerId50, @questionId12, 'Some other race,', 16, 'optionText'),
+    (@answerId51, @questionId12, 'Prefer not to say', 17, 'option'),
+    (@answerId52, @questionId13, 'Less than high school', 1, 'option'),
+    (@answerId53, @questionId13, 'High school/GED', 2, 'option'),
+    (@answerId54, @questionId13, 'Some college, no degree', 3, 'option'),
+    (@answerId55, @questionId13, 'Trade school', 4, 'option'),
+    (@answerId56, @questionId13, 'Associate degree', 5, 'option'),
+    (@answerId57, @questionId13, 'Bachelor’s degree', 6, 'option'),
+    (@answerId58, @questionId13, 'Graduate or professional degree', 7, 'option'),
+    (@answerId59, @questionId14, 'yes', 1, 'option'),
+    (@answerId60, @questionId14, 'no', 2, 'option'),
+    (@answerId61, @questionId15, 'Less than $10,000', 1, 'option'),
+    (@answerId62, @questionId15, '$10,000 but less than $15,000', 2, 'option'),
+    (@answerId63, @questionId15, '$15,000 but less than $25,000', 3, 'option'),
+    (@answerId64, @questionId15, '$25,000 but less than $35,000', 4, 'option'),
+    (@answerId65, @questionId15, '$35,000 but less than $50,000', 5, 'option'),
+    (@answerId66, @questionId15, '$50,000 but less than $75,000', 6, 'option'),
+    (@answerId67, @questionId15, '$75,000 but less than $100,000', 7, 'option'),
+    (@answerId68, @questionId15, '$100,000 but less than 150,000', 8, 'option'),
+    (@answerId69, @questionId15, '$150,000 but less than $200,000', 9, 'option'),
+    (@answerId70, @questionId15, '$200,000 or more', 10, 'option'),
+    (@answerId71, @questionId15, 'Don’t know/Prefer not to answer', 11, 'option')
 
 
 COMMIT TRAN;
