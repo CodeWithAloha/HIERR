3325fad904334be0784afb63eefe2102571d3b8c
avenmia <mavend2@gmail.com>
2023-03-07 16:48:15 -1000

    Updating polis survey page to read in the titles from the .env file

--- src/pages/polis.tsx
+++ src/pages/polis.tsx
@@ -1,26 +1,36 @@
 import { type NextPage } from "next";
 import { NextPageButtonLink } from "../UI/NextPageButtonLink";
 
+interface PolisSurvey {
+  id: string;
+  title: string;
+  description: string;
+}
+
 const Polis: NextPage = () => {
-  const surveys = process.env.NEXT_PUBLIC_POLIS_SURVEYS?.split(",");
+  const surveys: PolisSurvey[] = JSON.parse(
+    process.env.NEXT_PUBLIC_POLIS_SURVEYS ?? ""
+  );
 
   return (
     <div className="flex h-screen flex-col items-center bg-blue-default">
       <h1 className="my-6 text-white">
         Please select the Pol.is survey you wish to complete.
       </h1>
-      {surveys?.map((survey, index) => {
-        return (
-          <div className="mb-4" key={`survey-button-next-page-${index}`}>
-            <NextPageButtonLink
-              key="survey-{index}"
-              pageName="polissurvey"
-              msg={survey}
-              query={{ surveyId: survey }}
-            />
-          </div>
-        );
-      })}
+      <div className="justify-items-left flex flex-col">
+        {surveys?.map(({ id, title, description }, index) => {
+          return (
+            <div className="mb-4" key={`survey-button-next-page-${index}`}>
+              <NextPageButtonLink
+                key="survey-{index}"
+                pageName="polissurvey"
+                msg={title}
+                query={{ surveyId: id }}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
