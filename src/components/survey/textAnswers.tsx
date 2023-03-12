e8a5262cc12995cdbe9c8932880a75e52afe18da
l7ana <liana@saedesigngroup.com>
2023-03-12 11:51:07 -1000

    updated .env

--- src/components/survey/textAnswers.tsx
+++ src/components/survey/textAnswers.tsx
@@ -1,28 +1,13 @@
 interface TextAnswerProps {
   updateCurrentAnswer: (val: string) => void;
-  number?: boolean;
+  number?:boolean
 }
-
 
 export default function TextAnswer({ updateCurrentAnswer, number}: TextAnswerProps) {
 
   return (
     <>
         <input className="border-2 border-rose-500" type="text" id="textQuestion" name="textQuestion" pattern={number ? "^1?[0-9]{1,2}$1[0-9][0-9]" : ""} title={number ? "Please enter a number." : ""} onChange={(e) => updateCurrentAnswer(e.target.value)} />
-
-export default function TextAnswer({
-  updateCurrentAnswer,
-  number,
-}: TextAnswerProps) {
-  return (
-    <>
-      <input
-        className="form-input rounded"
-        type="text"
-        id="textQuestion"
-        name="textQuestion"
-        onChange={(e) => updateCurrentAnswer(e.target.value)}
-      />
     </>
-  );
+  )
 }
