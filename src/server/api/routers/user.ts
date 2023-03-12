ad85ed9e1fc983b7e26e28373adef435e91e984e
avenmia <mavend2@gmail.com>
2023-03-12 12:53:50 -1000

    Removing adding the census tract to the database

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -12,18 +12,7 @@ export const userRouter = createTRPCRouter({
       }
       const { id: userId } = ctx.session.user;
       const censusTract = input.censusTract;
-      const existingCensusTracts = await ctx.prisma.censusTract.findMany({
-        where: { id: censusTract },
-      });
-      if (existingCensusTracts.length === 0) {
-        await ctx.prisma.censusTract.create({
-          data: {
-            id: censusTract,
-            name: censusTract,
-          },
-        });
-      }
-      // TODO: Since each user is unique check if the user already has a zip code first
+
       return ctx.prisma.user.update({
         where: { id: userId },
         data: {
