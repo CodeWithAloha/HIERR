c19a60f131adcbd72110bf0a9ee091b1e00360a4
avenmia <mavend2@gmail.com>
2023-03-22 16:44:33 -1000

    Avenmia/react hook form migration (#104)
    * Adding react hook form library
    
    * Adding zip code validation
    
    * Adding eslint ignore in the correct place
    
    * Fixing zip code page styling
    
    * Removing dead code

--- src/components/zipcode.tsx
+++ src/components/zipcode.tsx
@@ -4,12 +4,27 @@ import { type NextPage } from "next";
 import { api } from "../utils/api";
 import { useCallback, useEffect, useState } from "react";
 import SelectedZipCode from "./selectedzipcode";
+import { useForm } from "react-hook-form";
+import stateZipCodes from "../data/2020_Zip_Code_Data.json";
+interface ZipCodeData {
+  zipCode: string;
+}
 
 const ZipCode: NextPage = () => {
   const [zipcode, setZipCode] = useState<string | null>(null);
   const [zipCodeComplete, setZipCodeComplete] = useState(false);
   const removeUserZipCode = api.zipcode.removeZipCode.useMutation();
   const zipCodeDB = api.zipcode.getUserZipCode.useQuery();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ZipCodeData>();
+
+  const onSubmit = handleSubmit((data) => {
+    setZipCodeComplete(true);
+    setZipCode(data.zipCode);
+  });
 
   useEffect(() => {
     if (zipCodeDB && zipCodeDB.data) {
@@ -19,10 +34,6 @@ const ZipCode: NextPage = () => {
       setZipCode(zipCodeDB.data?.zipcode);
     }
   }, [zipCodeDB.data?.zipcode]);
-
-  const handleSubmit = () => {
-    setZipCodeComplete(true);
-  };
 
   const handleRemoveZipCode = useCallback(() => {
     if (zipcode === null) {
@@ -34,38 +45,53 @@ const ZipCode: NextPage = () => {
     setZipCodeComplete(false);
   }, []);
 
+  const checkValidHawaiiZipCode = (val: string) => {
+    return stateZipCodes.includes(Number(val));
+  };
   return (
     <div className="flex h-screen flex-col items-center bg-[#3276AE]">
       {!zipCodeComplete ? (
-        <form
-          className="mt-6 overflow-hidden rounded bg-white px-8 py-6 shadow-lg"
-          onSubmit={handleSubmit}
-        >
-          <label htmlFor="zipcode" className="text-gray-700 mb-2 block text-sm">
-            Enter your zipcode:
-            <br />
-            <input
-              type="text"
-              id="zipcode"
-              name="zipcode"
-              className="text-gray-700 focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight shadow focus:outline-none"
-              required
-              value={zipcode === null ? "" : String(zipcode)}
-              onChange={(e) => setZipCode(e.target.value)}
-              style={{ color: "#4C4C4C" }}
-              pattern="[0-9]{5}"
-              title="The zipcode must be 5 digits."
-            />
-          </label>
-          <br />
-          <button
-            type="submit"
-            className="rounded-full border-blue-default bg-blue-default px-10 py-3 text-white no-underline transition hover:bg-blue-default hover:text-white"
-            style={{ background: "#17364F" }}
+        <>
+          <h1 className="mt-6 text-3xl font-bold text-white">
+            Please Enter Your Zip Code
+          </h1>
+          <p className="mt-6 w-3/5 text-center text-white">
+            This information will be used for the purposes of reporting on
+            demographic representation. This reporting ensures that our process
+            seeks to hear from as many perspectives in our community as possible
+          </p>
+          <form
+            className="mt-6 overflow-hidden rounded bg-white px-8 py-6 shadow-lg"
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onSubmit={onSubmit}
           >
-            Submit
-          </button>
-        </form>
+            <input
+              className="form-input rounded"
+              {...register("zipCode", {
+                required: true,
+                pattern: {
+                  value: /^[0-9]{5}/i,
+                  message: "The zip code must be 5 digits",
+                },
+                validate: {
+                  hawaiianZipCode: (v) =>
+                    checkValidHawaiiZipCode(v) ||
+                    "The zip code must be a valid Hawaiian zip code",
+                },
+              })}
+            />
+            <br />
+            {errors.zipCode && (
+              <>
+                <p style={{ color: "red" }}>{errors.zipCode.message}</p>
+              </>
+            )}
+            <input
+              className="mt-5 cursor-pointer rounded-full border-2 border-blue-default px-5 py-1 hover:bg-blue-default hover:text-white"
+              type="submit"
+            />
+          </form>
+        </>
       ) : (
         <SelectedZipCode
           zipcode={String(zipcode)}
