6c4310da2b665f114beb7a7bdce5895d8a5f7839
avenmia <mavend2@gmail.com>
2023-03-30 15:15:31 -1000

    Adding back button to Pol.is survey page

--- src/pages/polissurvey.tsx
+++ src/pages/polissurvey.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { api } from "../utils/api";
+import Link from "next/link";
 
 const PolisSurvey: NextPage = () => {
   const router = useRouter();
@@ -39,7 +40,17 @@ const PolisSurvey: NextPage = () => {
   }, []);
   return (
     <div className="flex h-max flex-col items-center bg-[#3276AE]">
-      {/* TODO: Fix Styling here */}
+      <h1 className="mt-6 text-3xl font-bold text-white">
+        Please Fill Out The Pol.is Survey
+      </h1>
+      <div className="mt-6 mb-6 flex justify-between align-middle">
+        <p className="mr-5 py-2 text-white">Return to polis survey selection</p>
+        <Link href={"./polis"}>
+          <button className="rounded-full bg-white/90 px-10 py-2 text-blue-default no-underline transition hover:bg-white hover:text-blue-darker">
+            Back
+          </button>
+        </Link>
+      </div>
       <div id="polis-container" style={{ width: "70vw", margin: "0 auto" }}>
         <div
           className="polis"
