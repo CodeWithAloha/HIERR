974d75b9d3d7ca3a963a68e165d86ed842e7d51d
Alexandra Haynes <sandamunteanua@yahoo.com>
2023-05-19 10:58:13 -1000

    Address PR review feedback

---
+++ src/components/WhatIsHierr.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const WhatIsHierr = () => {
+  return (
+    <>
+      <p className="mb-4 text-center">HIERR stands for</p>
+      <h2 className="pb-6 text-center text-3xl">
+        Hawai'i Economic Recovery & Resilience
+      </h2>
+      <p className="mb-4">
+        At HIERR we aim to enable actions toward a more resilient, equitable,
+        and sustainable economy. We want to learn from the economic impacts and
+        experiences of hardship associated with the COVID-19 pandemic to inform
+        and enable impactful actions. Rather than assume what these hardships
+        were like, we are conducting a statewide study to learn from diverse
+        stories told by residents and businesses across the state.
+      </p>
+      <h2 className="pb-4 text-2xl font-semibold">Our Objectives</h2>
+      <ul className="ml-8 mb-4 list-disc">
+        <li className="mb-2">
+          Identifying gaps and strategies to mitigate Hawaii's economic
+          vulnerabilities and hardships revealed during the COVID-19 pandemic;
+        </li>
+        <li className="mb-2">
+          Compiling specific projects/programs that address the identified gaps;
+        </li>
+        <li className="mb-2">
+          Aligning project/program needs to resources and enabling conditions
+          necessary for implementation;
+        </li>
+        <li className="mb-2">
+          Advancing pre-disaster economic recovery planning across all hazards.
+        </li>
+      </ul>
+      <h2 className="pb-4 text-2xl font-semibold">How you can help</h2>
+      <p className="pb-4">
+        You can make an impact for the economic development of Hawaii. By
+        answering the questions of this survey, you help us understand the
+        impact and challenges that the pandemic had on our communities. With
+        this valuable information we can identify strategies and projects to
+        build economic resilience for Hawaii.
+      </p>
+    </>
+  );
+};
+
+export default WhatIsHierr;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,15 +8,17 @@ module.exports = {
     extend: {
       backgroundImage: (theme) => ({
         "farmer-working": "url('/State-CEDS-Shawn-Uehira-p29.png')",
+        spectrum:
+          "linear-gradient(orange, transparent), linear-gradient(to top left, cyan, transparent), linear-gradient(to top right, purple, transparent)",
       }),
     },
     colors: {
-      // green: "#1A9432",
-      // blue: {
-      //   lighter: "#COD8ED",
-      //   default: "#3276AE",
-      //   darker: "#17364F",
-      // },
+      green: "#1A9432",
+      blue: {
+        lighter: "#COD8ED",
+        default: "#3276AE",
+        darker: "#17364F",
+      },
       white: "#FFFFFF",
       gray: "#4C4C4C",
     },
