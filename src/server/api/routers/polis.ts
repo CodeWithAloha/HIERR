72f8e64f336691c80c442a5181997b2d8d1371d2
avenmia <mavend2@gmail.com>
2024-03-09 17:05:45 -1000

    Implementing polis filter

--- src/server/api/routers/polis.ts
+++ src/server/api/routers/polis.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { PolisSurveys, SurveyRules } from "@prisma/client";
 
 export const polisRouter = createTRPCRouter({
   getSurveys: publicProcedure.query(async ({ ctx }) => {
@@ -10,37 +11,57 @@ export const polisRouter = createTRPCRouter({
     }
     const { id: userId } = ctx.session.user;
 
-    // const userSurveyAnswers = ctx.prisma.userSurveyAnswers.findMany({
-    //   where: { userId: userId },
-    //   select: {
-    //     questionId: true,
-    //     answerId: true,
-    //   },
-    // });
+    const userSurveyAnswers = await ctx.prisma.userSurveyAnswers.findMany({
+      where: { userId: userId },
+      select: {
+        questionId: true,
+        answerId: true,
+      },
+    });
 
-    // const allSurveys = ctx.prisma.polisSurveys.findMany({
-    //   select: {
-    //     id: true,
-    //     title: true,
-    //     description: true,
-    //   }
-    // });
-
-    // const surveyRules = ctx.prisma.surveyRules.findMany({
-    //   select: {
-    //     id: true,
-    //     surveyId: true,
-    //     questionId: true,
-    //     requiredAnswerId: true
-    //   }
-    // });
-
-    return ctx.prisma.polisSurveys.findMany({
+    const allSurveys: PolisSurveys[] = await ctx.prisma.polisSurveys.findMany({
       select: {
         id: true,
         title: true,
         description: true,
       },
     });
+
+    const surveyRules: SurveyRules[] = await ctx.prisma.surveyRules.findMany({
+      select: {
+        id: true,
+        surveyId: true,
+        questionId: true,
+        requiredAnswerId: true,
+      },
+    });
+
+    const surveysWithRules = new Set(surveyRules.map((rule) => rule.surveyId));
+
+    const filteredSurveys: PolisSurveys[] = allSurveys.filter(
+      (survey) => !surveysWithRules.has(survey.id)
+    );
+
+    surveyRules.map((rule) => {
+      const surveyId = rule.surveyId;
+      const questionId = rule.questionId;
+      const requiredAnswerId = rule.requiredAnswerId;
+
+      // Returns all the answers for a certain question
+      const userAnswer = userSurveyAnswers.find(
+        (answer) => answer.questionId === questionId
+      );
+
+      if (userAnswer && userAnswer.answerId === requiredAnswerId) {
+        const filteredSurvey = allSurveys.find(
+          (survey) => survey.id === surveyId
+        );
+        if (filteredSurvey !== undefined) {
+          filteredSurveys.push(filteredSurvey);
+        }
+      }
+    });
+
+    return filteredSurveys;
   }),
 });
