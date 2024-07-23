bf24afea328ac66d99cb2ae4bb6724613aa969b5
avenmia <mavend2@gmail.com>
2024-07-22 14:20:44 -1000

    Removing google maps and replacing with esri-leaflet-geocoder

--- src/components/AddressSearch.tsx
+++ src/components/AddressSearch.tsx
@@ -1,131 +1,167 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, ChangeEvent } from "react";
 import { LoadScript, Autocomplete, Libraries } from "@react-google-maps/api";
+import ZipCodesGeojson from "../data/zipcodes.json";
+import CensusTractGeojson from "../data/census-tracts_min.json";
+import PlanningAreaGeojson from "../data/Plan_Areas.json";
+import * as turf from "@turf/turf";
+import { FeatureCollection, Feature, Polygon } from "geojson";
+import * as ELG from "esri-leaflet-geocoder";
 
-interface GoogleMapsResponseData {
-  results: google.maps.places.PlaceResult[];
+interface AddressSuggestion {
+  text: string;
+  magicKey: string;
 }
 
-interface CensusTractData {
-  result: {
-    geographies: {
-      "Census Tracts": {
-        TRACT: string;
-        NAME: string;
-        BASENAME: string;
-      }[];
-    };
-  };
+interface AddressResponse {
+  results: Address[];
+}
+interface Address {
+  latlng: LatLng;
+  text: string;
+}
+
+interface LatLng {
+  lat: number;
+  lng: number;
 }
 
 const AddressSearch: React.FC = () => {
-  const [address, setAddress] = useState("");
+  const [query, setQuery] = useState<string>("");
+  const [suggestions, setSuggestions] = useState<AddressSuggestion[]>([]);
   const [censusTract, setCensusTract] = useState<string | null>(null);
   const [zipCode, setZipCode] = useState<string | null>(null);
   const [planningRegion, setPlanningRegion] = useState<string | null>(null);
-  const autocompleteRef = useRef<google.maps.places.Autocomplete | null>(null);
-  const libraries: Libraries = ["places"];
+  const [location, setLocation] = useState<Address | null>(null);
+  const apiToken = process.env.NEXT_PUBLIC_SEARCH_API;
 
-  const fetchCensusTract = async (latitude: number, longitude: number) => {
-    const url = `https://geocoding.geo.census.gov/geocoder/geographies/coordinates?x=${longitude}&y=${latitude}&benchmark=2020&vintage=2020&format=json`;
+  const boundaryBox = {
+    xmin: -162.171387,
+    ymin: 18.367807,
+    xmax: -153.404297,
+    ymax: 23.131708,
+  };
 
-    try {
-      const response = await fetch(url);
-      const data = (await response.json()) as CensusTractData;
-      console.log("Census Tract Data is:", data);
-      if (
-        data !== undefined &&
-        data.result !== undefined &&
-        data.result.geographies !== undefined
-      ) {
-        // Safely access the 'TRACT' property by checking if 'Census Tracts' and its first element exist
-        setCensusTract(
-          data.result.geographies["Census Tracts"]?.[0]?.["TRACT"] ??
-            "Not Found"
-        );
-      }
-    } catch (error) {
-      console.error("Error fetching census tract data:", error);
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setQuery(value);
+
+    if (value.length > 2) {
+      const geocodeService = ELG.geocodeService({ apikey: apiToken });
+
+      geocodeService
+        .suggest()
+        .text(value)
+        .within([
+          [boundaryBox.ymin, boundaryBox.xmin],
+          [boundaryBox.ymax, boundaryBox.xmax],
+        ])
+        .run((err: any, response: any) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          setSuggestions(
+            response.suggestions.map((suggestion: any) => ({
+              text: suggestion.text,
+              magicKey: suggestion.magicKey,
+            }))
+          );
+        });
+    } else {
+      setSuggestions([]);
     }
   };
 
-  const handlePlaceChanged = async () => {
-    if (autocompleteRef.current) {
-      const place = autocompleteRef.current?.getPlace();
-      console.log("Place is:", place);
-      if (place.formatted_address) {
-        setAddress(place.formatted_address);
-        console.log("Address is:", place.formatted_address);
+  const handleSuggestionClick = (suggestion: AddressSuggestion) => {
+    const geocodeService = ELG.geocodeService({ apikey: apiToken });
+    const geocode = geocodeService.geocode();
+    const geocodeText = geocode.text(suggestion.text);
 
-        const location = place.geometry?.location;
-        if (location) {
-          const lat = location.lat();
-          const lng = location.lng();
-          console.log("Lat is:", lat);
-          console.log("Lng is:", lng);
+    geocodeText.run((err: any, result: AddressResponse) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const { lat, lng } = result.results[0]?.latlng;
 
-          try {
-            const response = await fetch(
-              `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${
-                process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ""
-              }`
-            );
-            const data = (await response.json()) as GoogleMapsResponseData;
-            console.log("Data is:", data);
+      setLocation({ text: suggestion.text, latlng: result.results[0]?.latlng });
+      setDemographicData(lat, lng);
+      setSuggestions([]);
+      setQuery(suggestion.text);
+    });
+  };
 
-            const addressComponents = data.results[0]?.address_components;
-            console.log("Address components are:", addressComponents);
-            const zipCodeComponent = addressComponents?.find(
-              (component: google.maps.GeocoderAddressComponent) =>
-                component.types.includes("postal_code")
-            );
-            await fetchCensusTract(lat, lng);
-
-            setZipCode(
-              zipCodeComponent ? zipCodeComponent.long_name : "Not found"
-            );
-          } catch (error) {
-            console.error("Error fetching geocode data:", error);
-          }
+  const setDemographicData = (lat: number, lng: number) => {
+    const point = turf.point([lng, lat]);
+    let zip = "";
+    let censusTract = "";
+    let featurePlanningRegion = "";
+    turf.featureEach(
+      ZipCodesGeojson as FeatureCollection<Polygon>,
+      (currentFeature, featureIndex) => {
+        if (turf.booleanPointInPolygon(point, currentFeature)) {
+          zip = currentFeature.properties.ZIP ?? "Not Found";
         }
       }
-    }
+    );
+    turf.featureEach(
+      CensusTractGeojson as FeatureCollection<Polygon>,
+      (currentFeature, featureIndex) => {
+        if (turf.booleanPointInPolygon(point, currentFeature)) {
+          censusTract = currentFeature.properties.name20 ?? "Not Found";
+        }
+      }
+    );
+    turf.featureEach(
+      PlanningAreaGeojson as FeatureCollection<Polygon>,
+      (currentFeature, featureIndex) => {
+        if (turf.booleanPointInPolygon(point, currentFeature)) {
+          console.log("FOUND PLANNING REGION!");
+          featurePlanningRegion =
+            currentFeature.properties.COMMUNITY_ ?? "Not Found";
+        }
+      }
+    );
+
+    setZipCode(zip);
+    setCensusTract(censusTract);
+    setPlanningRegion(featurePlanningRegion);
   };
 
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
-      libraries={libraries}
-    >
-      <div className="flex h-screen items-center justify-center">
-        <div className="flex flex-col items-center">
-          <Autocomplete
-            onLoad={(autocomplete) => {
-              autocompleteRef.current = autocomplete;
-            }}
-            onPlaceChanged={() => {
-              handlePlaceChanged().catch(console.error);
-            }}
-          >
-            <input
-              type="text"
-              placeholder="Search for an address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="border-gray-300 focus:ring-blue-500 focus:border-transparent w-96 rounded-md border p-2 shadow-sm focus:outline-none focus:ring-2"
-            />
-          </Autocomplete>
-          <div className="mt-4 text-center">
-            {zipCode && <p className="text-white">ZIP Code: {zipCode}</p>}
-            {censusTract && (
-              <p className="text-white">Census Tract: {censusTract}</p>
-            )}
-            {planningRegion && (
-              <p className="text-white">Planning Region: {planningRegion}</p>
-            )}
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center">
+        <input
+          type="text"
+          value={query}
+          onChange={handleInputChange}
+          placeholder="Enter address"
+          className="border-gray-300 w-full rounded border p-2"
+        />
+        {suggestions.length > 0 && (
+          <div className="border-gray-300 z-10 mt-20 rounded border bg-white">
+            {suggestions.map((suggestion, index) => (
+              <div
+                key={index}
+                className="hover:bg-gray-100 cursor-pointer p-2"
+                onClick={() => handleSuggestionClick(suggestion)}
+              >
+                {suggestion.text}
+              </div>
+            ))}
           </div>
+        )}
+        <div className="mt-4 text-center">
+          {zipCode && <p className="text-white">ZIP Code: {zipCode}</p>}
+          {censusTract && (
+            <p className="text-white">Census Tract: {censusTract}</p>
+          )}
+          {planningRegion && (
+            <p className="text-white">Planning Region: {planningRegion}</p>
+          )}
         </div>
       </div>
-    </LoadScript>
+    </div>
   );
 };
 
